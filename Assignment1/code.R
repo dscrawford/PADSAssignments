f0634cf53446d2badd18f5a1aba1e2d431422a7c
Daniel Crawford <daniel.sc.crawford@gmail.com>
2021-02-11 21:08:04 -0600

    Progress on project

--- Assignment1/code.R
+++ Assignment1/code.R
@@ -1,3 +1,5 @@
+install.packages(c('mltools'))
+
 df <- read.csv('housing.csv')
 View(df)
 
@@ -6,20 +8,30 @@ require(ggplot2)
 
 # Percentage of null values
 sapply(df, function(x) sum(is.na(x))) / nrow(df)
-# Remove unwanted null values
-df <- na.omit(df[-ncol(df)])
 
+# Remove unwanted null values
+df <- na.omit(df)
+
+# Display histograms of each variable
+lapply(colnames(df[-which(colnames(df) == 'ocean_proximity')]), 
+       FUN=function(x) hist(as.vector(df[[x]]),
+                            main=paste('Histogram of ', x),
+                            xlab=x,
+                            breaks=100)
+       )
+
+# One hot encode ocean
+library(mltools)
+library(data.table)
+
+df$ocean_proximity
+# Create correlation plot
 M <- cor(df)
 corrplot(M, method="circle")
 
 # Geographical map
+# put code here
 
-# Histogram
-hist(df$housing_median_age, breaks = 100, main='Histogram of housing_median_age')
-hist(df$total_rooms, breaks=100, main='Histogram of total_rooms')
-
-ggplot(df, aes(x = length(df))) +
-  geom_histogram(fill = "white", colour = "black")
 
 
 plot(housing_median_age, median_income, data=df)
