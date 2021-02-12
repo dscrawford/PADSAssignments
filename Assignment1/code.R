6cc6052ac59851a81ae2cebe481e6d7217cb4e82
Daniel Crawford <daniel.sc.crawford@gmail.com>
2021-02-11 20:17:05 -0600

    Starting on code for assignment 1

--- Assignment1/code.R
+++ Assignment1/code.R
@@ -1,14 +1,27 @@
 df <- read.csv('housing.csv')
 View(df)
 
+require(corrplot)
+require(ggplot2)
+
 # Percentage of null values
 sapply(df, function(x) sum(is.na(x))) / nrow(df)
-
-# Remove unwanted rows
+# Remove unwanted null values
 df <- na.omit(df[-ncol(df)])
 
-require(corrplot)
 M <- cor(df)
 corrplot(M, method="circle")
 
+# Geographical map
 
+# Histogram
+hist(df$housing_median_age, breaks = 100, main='Histogram of housing_median_age')
+hist(df$total_rooms, breaks=100, main='Histogram of total_rooms')
+
+ggplot(df, aes(x = length(df))) +
+  geom_histogram(fill = "white", colour = "black")
+
+
+plot(housing_median_age, median_income, data=df)
+
+lm.fit(df$median_income~df$housing_median_age+df$total_rooms, data=df)
