1423e1463611c96ddfe6e5856089b6e8d8b3edcf
Abhishek Thurlapati <abhishekthurlapati@Abhisheks-MBP.lan>
2021-04-08 00:25:14 -0500

    Assignment3 added

---
+++ Assignment3/Assignment3.R
@@ -0,0 +1,29 @@
+#install.packages('sentimentr')
+#install.packages('srtweet')
+library(rtweet)
+library(sentimentr)
+library(ggplot2)
+
+#Grab tweets
+tweets <- search_tweets(q = "#corona", n = 500, 
+                        include_rts = FALSE, lang = "en")
+
+
+#Cleans text
+tweets$strippedtext <- gsub("http.*", "", tweets$text)
+tweets$strippedtext <- gsub("https.*", "", tweets$text)
+
+#Get sentiment with sentimentr (tweet level)
+sentiment = sentiment_by(tweets$strippedtext)
+sentiment
+
+View(tweets)
+
+#Sentiment analysis
+summary(sentiment$ave_sentiment)
+
+qplot(sentiment$ave_sentiment,   geom="histogram",binwidth=0.1,main="Sentiment Frequencies of Tweets on Corona")
+
+#Can do overall level sentiment if i join all the tweets together of a certain time i guess
+#Or can do sentiment of tweets at a certain time and find the avg sentiment of that time period. 
+#We need to explore the rtweets and sentimentr package to see if there are other stuff we can analyze
